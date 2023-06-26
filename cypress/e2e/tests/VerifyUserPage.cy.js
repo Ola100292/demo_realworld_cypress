9556086126219484d57b8bc30fed49f96a0e2b20
Ola <112119427+Ola100292@users.noreply.github.com>
2023-06-26 13:17:20 +0200

    finished working assertion for settings

--- cypress/e2e/tests/VerifyUserPage.cy.js
+++ cypress/e2e/tests/VerifyUserPage.cy.js
@@ -1,3 +1,4 @@
+
 import UserPage, { openSettingsPage } from "../../pages/UserPage";
 /// <reference types="cypress"/>
 
@@ -15,7 +16,7 @@ describe('Pom Implementation - Logged user with valid acc', () => {
     UserPage.elementsForAssertion.loginCheck().should('be.visible');
     cy.visit('')
     UserPage.elementsForAssertion.yourFeedFunction().should('be.visible');
-   
+    UserPage.elementsForAssertion.newArticleBtn().should('match', '[ui-sref-active="active"]');
 
   })
 
@@ -24,7 +25,14 @@ describe('Pom Implementation - Logged user with valid acc', () => {
 
     UserPage.settingsElements.profilePicture().should('be.visible')
       .and('have.prop', 'readOnly', false)
-      .and('have.prop', 'required', false)
+      .and('have.prop', 'required', false);
+    UserPage.settingsElements.usernameText().should('match', '[ng-model="$ctrl.formData.username"]');
+    UserPage.settingsElements.aboutUserText().should('match', '[ng-model="$ctrl.formData.bio"]');
+    UserPage.settingsElements.emailText().should('match', '[ng-model="$ctrl.formData.email"]');
+    UserPage.settingsElements.newPasswordText().should('match', '[ng-model="$ctrl.formData.password"]');
+    UserPage.settingsElements.updateSettingsBtn().should('contain', 'Update Settings')
+    .and('have.css', 'background-color', 'rgb(92, 184, 92)');
+    UserPage.settingsElements.logOutBtn().should('contain', 'Or click here to logout.')
 
   })
 
--- cypress/pages/UserPage.js
+++ cypress/pages/UserPage.js
@@ -3,7 +3,8 @@ class UserPage {
     elementsForAssertion = {
         loginCheck: () => cy.get('[href="#/@olaTest"]'),
         activeFeed: () => cy.get('[class="nav-link active"]').eq(1),
-        yourFeedFunction: () => cy.contains('Your Feed')
+        yourFeedFunction: () => cy.contains('Your Feed'),
+        newArticleBtn: () => cy.get('[href="#/editor/"]')
     }
     settingsElements =
         {
@@ -13,8 +14,8 @@ class UserPage {
             aboutUserText: () => cy.get('[placeholder="Short bio about you"]'),
             emailText: () => cy.get('[placeholder="Email"]'),
             newPasswordText: () => cy.get('[placeholder="New Password"]'),
-            updateSettingsBtn: () => cy.get('[class="btn-primary pull-xs-right"]')
-
+            updateSettingsBtn: () => cy.get('[type="submit"]'),
+            logOutBtn: () => cy.get('[ng-click="$ctrl.logout()"]')
         }
 
     yourFeedFunctionClick() {
