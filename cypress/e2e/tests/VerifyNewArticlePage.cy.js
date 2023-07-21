2139484064a424a4fa71f38c66bc3bd397dfc586
Ola <112119427+Ola100292@users.noreply.github.com>
2023-07-21 11:54:06 +0200

    prepare for new test - validation

--- cypress/e2e/tests/VerifyNewArticlePage.cy.js
+++ cypress/e2e/tests/VerifyNewArticlePage.cy.js
@@ -1,22 +1,29 @@
 import NewArticlePage from "../../pages/NewArticlePage"
 
 describe('Pom Implementation - Create new article', () => {
-    beforeEach(function () {
-      Cypress.session.clearAllSavedSessions()
+  beforeEach(function () {
+    cy.visit('')
+    cy.log('create token for valid user')
+    cy.loginWithApi()
+  })
+  afterEach(function () {
+    Cypress.session.clearAllSavedSessions()
 
-      cy.visit('')
-      cy.log('create token for valid user')
-      cy.loginWithApi()
-    })
-it('Should check new article section', () => {
+  })
+  it('Should check new article section', () => {
 
     NewArticlePage.editorBtnClick()
     NewArticlePage.editorElements.articleTitle().should('be.visible').and('not.be.null')
     NewArticlePage.editorElements.shortInfo().should('be.visible').and('match', '[type="text"]')
     NewArticlePage.editorElements.articleText().should('be.visible').and('have.prop', 'readOnly', false)
-    .and('have.prop', 'required', false)
+      .and('have.prop', 'required', false)
     NewArticlePage.editorElements.tags().should('be.visible')
 
-})
+  })
+
+  it('Should check validation for creating article', () => {
+    NewArticlePage.editorBtnClick()
+    
+  })
 
 })
